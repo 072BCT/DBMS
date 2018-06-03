a13b54e880035c008448b0f7f362307fea8426eb
Sushant Gautam <susant.gautam@gmail.com>
2018-06-03 13:07:27 +0545

    updates database and er diagram

--- college/admin.py
+++ college/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Programme
+from .models import *
 
 # Register your models here.
 admin.site.register(Programme)
+admin.site.register(Batch)
+admin.site.register(Semester)
+admin.site.register(Expert)
+admin.site.register(Topic)
+admin.site.register(Teacher)
+admin.site.register(Subject)
+
--- college/models.py
+++ college/models.py
@@ -1,7 +1,13 @@
+import datetime
 from django.db import models
 
-#pw - dbms1234
-# Create your models here.
+
+# pw - dbms1234
+
+
+Degree_Choices = {('PhD', 'PhD'), ('MSc', 'MSc'), ('Bachelors', 'Bachelors'), ('Diploma', 'Diploma')}
+Affiliation_Choices = {('Permanent', 'Permanent'), ('Contract', 'Contract'), ('Visiting', 'Visiting')}
+
 
 class Programme(models.Model):
     name = models.CharField(max_length=40)
@@ -9,55 +15,65 @@ class Programme(models.Model):
     def __str__(self):
         return self.name
 
+
 class Batch(models.Model):
-    programme = models.ForeignKey(Programme, on_delete= models.DO_NOTHING)
-    no_stud = models.IntegerField()
+    year = models.CharField(max_length=40, default=datetime.date.today().strftime("%Y"))
+    programme = models.ForeignKey(Programme, on_delete=models.DO_NOTHING)
+    number_of_students = models.IntegerField(default='48')
+
+    def __str__(self):
+        return self.programme.name + " : " + self.year
+
 
 class Semester(models.Model):
-    batch = models.ForeignKey(Batch, on_delete= models.CASCADE)
-    year = models.CharField(max_length=10)
-    part = models.CharField(max_length=10)
+    name = models.CharField(max_length=40, blank=True, null=True)
 
-class Course(models.Model):
-    semester = models.ForeignKey(Semester, on_delete= models.CASCADE)
-    int_marks = models.IntegerField()
-    ext_marks = models.IntegerField()
-    total_marks = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    # programme = models.ForeignKey(Programme, on_delete= models.PROTECT)
+    name = models.CharField(max_length=40)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=30)
+    affiliated_institute = models.CharField(max_length=10)
+    teaching_experience_years = models.IntegerField(default='1')
+
+    upper_degree = models.CharField(max_length=30, choices=Degree_Choices, default='MSc')
+    aff_type = models.CharField(max_length=30, choices=Affiliation_Choices, default='Permanent')
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=40, blank=True, null=True)
+    program = models.ForeignKey(Programme, on_delete=models.CASCADE, null=True)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    subject_teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True)
+    program_code = models.CharField(max_length=40, blank=True, null=True)
+    int_marks = models.IntegerField(default="4")
+    int_marks = models.IntegerField(default="40")
+    ext_marks = models.IntegerField(default="60")
+
     # total_marks = int_marks + ext_marks
 
     # def total_marks(self):
     # return self.int_marks + self.ext_marks
 
-class Teacher(models.Model):
-    #programme = models.ForeignKey(Programme, on_delete= models.PROTECT)
-    course = models.ForeignKey(Course, on_delete= models.PROTECT)
-    name = models.CharField(max_length = 40)
-    phone = models.CharField(max_length= 20)
-    email = models.CharField(max_length=30)
-    aff_campus = models.CharField(max_length=10)
-    experience = models.IntegerField()
-    exp_on_sub = models.IntegerField()
-    upper_deg = models.CharField(max_length=20)
-    aff_type = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name + "  :  " + self.program.name + " - " + self.semester.name
 
-    #(Permanent, Contract, Visiting)
 
 class Topic(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.PROTECT)
+    course = models.ForeignKey(Subject, on_delete=models.PROTECT)
     name = models.CharField(max_length=40)
 
+
 class Expert(models.Model):
-    #programme = models.ForeignKey(Programme, on_delete= models.PROTECT)
-    topic = models.ForeignKey(Topic, on_delete= models.PROTECT)
-    name = models.CharField(max_length = 40)
-    phone = models.CharField(max_length= 20)
-    email = models.CharField(max_length=30)
+    # programme = models.ForeignKey(Programme, on_delete= models.PROTECT)
+    topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
+    name = models.CharField(max_length=40)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=30)
     aff_campus = models.CharField(max_length=10)
-    upper_deg = models.CharField(max_length=20)
-    aff_type = models.CharField(max_length=30)
-
-
-
-
-
-
+    upper_degree = models.CharField(max_length=30, choices=Degree_Choices, default='MSc')
+    aff_type = models.CharField(max_length=30, choices=Affiliation_Choices, default='Permanent')
