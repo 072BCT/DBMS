f03ffdd854425f22395c57868da5707ff8cf23d3
Sushant Gautam <susant.gautam@gmail.com>
2018-06-03 18:04:49 +0545

    custom admin page header and text

--- college/admin.py
+++ college/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import User, Group
+from django.utils.translation import gettext_lazy
+
 from .models import *
+
+
+# https://docs.djangoproject.com/en/2.0/ref/contrib/admin/#modeladmin-objects
+
+class AdminSite(AdminSite):
+    # Text to put at the end of each page's <title>.
+    site_title = gettext_lazy('IOE')
+
+    # Text to put in each page's <h1>.
+    site_header = gettext_lazy('IOE PUL')
+
+    # Text to put at the top of the admin index page.
+    index_title = gettext_lazy('IOE PUL MSC')
+
+
+admin.site = AdminSite()
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Group, GroupAdmin)
 
 # Register your models here.
 admin.site.register(Programme)
@@ -9,4 +33,6 @@ admin.site.register(Expert)
 admin.site.register(Topic)
 admin.site.register(Teacher)
 admin.site.register(Subject)
+
+
 
