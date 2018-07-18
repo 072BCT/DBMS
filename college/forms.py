df5e8e74d56bcbfcfee3b4d81b338094f25ae6f0
Sushant Gautam <susant.gautam@gmail.com>
2018-07-18 15:26:01 +0545

    Export problemfixed on Basanta sir

--- college/forms.py
+++ college/forms.py
@@ -5,27 +5,12 @@ from college.models import AssignSubjectTeacher, Batch, Programme, Year
 Semester_Choices = {("", '----'), ('First', 'First'), ('Second', 'Second'), ('Third', 'Third'), ('Fourth', 'Fourth')}
 
 
-def set_field_html_name(cls, new_name):
-    """
-    This creates wrapper around the normal widget rendering,
-    allowing for a custom field name (new_name).
-    """
-    old_render = cls.widget.render
-
-    def _widget_render_wrapper(name, value, attrs=None):
-        return old_render(new_name, value, attrs)
-
-    cls.widget.render = _widget_render_wrapper
-
-
 class ExportForm(forms.ModelForm):
     year = forms.ModelChoiceField(queryset=Year.objects.all(), required=False)
     batch = forms.ModelChoiceField(queryset=Batch.objects.all(), required=False)
     programme = forms.ModelChoiceField(queryset=Programme.objects.all(), required=False)
     part = forms.ChoiceField(choices={("", '----'), ('Even', 'Even'), ('Odd', 'Odd')}, required=False)
     semester = forms.ChoiceField(choices=Semester_Choices, required=False)
-
-    set_field_html_name(year, 'Academic Year')
 
     class Meta:
         model = AssignSubjectTeacher
